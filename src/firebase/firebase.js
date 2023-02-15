68c05f99defec16346f8ffa20e0da136f0831d1d
lte94 <ggrgtvv@naver.com>
2023-02-15 20:12:26 +0900

    ✨ feat(CreatePost,PostList) : New feature by @lte94

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -12,4 +13,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-export { app, auth };
+const storageService = getStorage(app);
+export { app, auth, storageService };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from "next/app";
-import "../globals.css";
 import Layout from "../components/Layout/Layout";
 import { QueryClient, QueryClientProvider } from "react-query";
 
